830ba152e4b71f34d6b9f8d439d2e480b804ff07
18507699964 <627311610@qq.com>
2019-11-11 15:02:32 +0800

    2019-11-11：设备路由表更新

---
+++ Monitor/monitor_front/src/router/modules/devices.js
@@ -0,0 +1,29 @@
+
+import Layout from '@/layout'
+
+const devicesRouter = {
+  path: '/devices',
+  component: Layout,
+  redirect: 'noRedirect',
+  name: 'Devices',
+  meta: {
+    title: '设备',
+    icon: 'list'
+  },
+  children: [
+    {
+      path: 'list',
+      component: () => import('@/views/devices/list'),
+      name: 'DeviceList',
+      meta: { title: '设备列表', noCache: true }
+    },
+    {
+      path: 'interface',
+      component: () => import('@/views/devices/interface'),
+      name: 'DeviceInterface',
+      meta: { title: '设备端口', noCache: true }
+    }
+  ]
+}
+
+export default devicesRouter
