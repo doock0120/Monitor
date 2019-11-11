2619d1de44b6f1caecca1a9edc80e17dddd78c8e
doock0120 <hsuche0120@outlook.com>
2019-11-11 15:13:49 +0800

    项目工程创建完成

--- Monitor/monitor_front/src/router/index.js
+++ Monitor/monitor_front/src/router/index.js
@@ -12,6 +12,9 @@ import chartsRouter from './modules/charts'
 import tableRouter from './modules/table'
 import nestedRouter from './modules/nested'
 
+import newsRouter from './modules/news'
+import deptRouter from './modules/department'
+
 /**
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -189,6 +192,8 @@ export const asyncRoutes = [
   chartsRouter,
   nestedRouter,
   tableRouter,
+  newsRouter,
+  deptRouter,
 
   {
     path: '/example',
