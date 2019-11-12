05b80e0b9c2d8c3fddd9d7097628b7aac83f110c
18507699964 <627311610@qq.com>
2019-11-12 10:01:49 +0800

    2019--11--12：完成设备列表查询功能

--- Monitor/monitor_front/src/views/devices/list.vue
+++ Monitor/monitor_front/src/views/devices/list.vue
@@ -4,16 +4,38 @@
     <!--搜索条件区-->
     <div class="filter-container">
       <el-input
-        v-model="listQuery.deptName"
-        placeholder="部门名称"
-        style="width: 200px"
+        v-model="listQuery.devName"
+        placeholder="设备名称"
+        style="width: 150px"
         class="filter-item"
         @keyup.enter.native="handleFilter"
       />
-      <el-select v-model="listQuery.personCount" class="filter-item" placeholder="人员数量" @change="handleFilter">
-        <el-option key="0" value="0" label="&#45;&#45;请选择&#45;&#45;" />
-        <el-option v-for="item in personCount" :key="item.key" :value="item.key" :label="item.value" />
+      <el-input
+        v-model="listQuery.devIP"
+        placeholder="设备IP"
+        style="width: 150px"
+        class="filter-item"
+        @keyup.enter.native="handleFilter"
+      />
+      <el-select v-model="listQuery.devType" class="filter-item" placeholder="设备类型" style="width: 150px" @change="handleFilter">
+        <el-option key="0" value="0" label="全部" />
+        <el-option v-for="item in devType" :key="item.key" :value="item.key" :label="item.value" />
       </el-select>
+      <el-input
+        v-model="listQuery.devOperator"
+        placeholder="包机人"
+        style="width: 150px"
+        class="filter-item"
+        @keyup.enter.native="handleFilter"
+      />
+      <el-button
+        class="filter-item"
+        style="margin-left: 10px;"
+        icon="el-icon-search"
+        @click="handleFilter"
+      >
+        查询
+      </el-button>
 
       <el-button
         class="filter-item"
@@ -26,15 +48,15 @@
       </el-button>
     </div>
 
-    &lt;!&ndash;部门数据表格&ndash;&gt;
+    <!--设备列表数据表格-->
     <el-table v-loading="listLoading" :data="pageData" border>
-      <el-table-column sortable prop="id" label="编号" align="center" width="100" />
-      <el-table-column sortable prop="peoples" label="人员数量" align="center" width="120">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.peoples | hotTagType">{{ scope.row.peoples }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column prop="deptName" label="部门名称" min-width="500" />
+      <el-table-column sortable prop="id" label="编号" align="center" min-width="75" />
+      <el-table-column prop="devName" label="设备名称" align="center" />
+      <el-table-column prop="devIP" label="设备IP" align="center" min-width="130" />
+      <el-table-column prop="devType" label="设备类型" align="center" />
+      <el-table-column prop="devOperator" label="包机人" align="center" />
+      <el-table-column prop="devAddress" label="设备地址" min-width="300" />
+
       <el-table-column label="操作" align="center" width="230" class-name="small-padding fixed-width">
         <template slot-scope="{row}">
           <el-button type="primary" size="mini" @click="handleUpdate(row)">
@@ -60,49 +82,101 @@
 </template>
 
 <script>
+
 import Pagination from '@/components/Pagination'
+import { fetchList } from '@/api/devlist'
 
 export default {
   components: { Pagination },
-  filters: {
-    hotTagType(value) {
-      return value >= 25 ? 'danger' : (value >= 15 ? 'warning' : 'success')
-    }
-  },
   data() {
     return {
-      // 控制等待画面显示与否
-      listLoading: false,
 
+      // 控制等待画面显示与否
+      listLoading: true,
+
+      // 获取设备总数据列表
       deviceList: [],
       total: 0,
+      // 当前页数据
+      pageData: [],
 
       // 查询条件
       listQuery: {
         page: 1,
-        limit: 10
+        limit: 10,
+        devName: '',
+        devIP: '',
+        devOperator: '',
+        devType: ''
       },
-
-      // 当前页数据
-      pageData: []
+      devType: [
+        { key: 1, value: '路由器' },
+        { key: 2, value: '交换机' }
+      ]
     }
   },
   mounted() {
-    this.freshList()
+    this.getList()
   },
   methods: {
-    freshList() {
-
-    },
-
     getList() {
+      // 显示等待画面
+      this.listLoading = true
 
+      // 初始获取所有设备数据列表
+      fetchList(this.listQuery).then(response => {
+        if (response.code === 20000) {
+          this.deviceList = response.data.items
+          this.total = response.data.items.length
+
+          // 数据分页
+          this.paging()
+
+          // 1.5s后关闭等待画面
+          setTimeout(() => {
+            this.listLoading = false
+          }, 0.5 * 1000)
+        }
+      }).catch(err => {
+        console.log(err) // 这里catch到错误timeout
+      })
+    },
+    // 筛选、分页方法
+    paging() {
+      const { page, limit, devName, devIP, devOperator, devType } = this.listQuery
+
+      // 对deptList数据进行过滤
+      let filterData = []
+
+      filterData = this.deviceList.filter(item => {
+        if (devType === 1 && item.devType !== '路由器') return false
+        if (devType === 2 && item.devType !== '交换机') return false
+        if (devName && item.devName.indexOf(devName) === -1) return false
+        if (devIP && item.devIP.indexOf(devIP) === -1) return false
+        if (devOperator && item.devOperator.indexOf(devOperator) === -1) return false
+        return true
+      })
+
+      // 设置数据总数为过滤后的总数据量
+      this.total = filterData.length
+
+      // 对deptList数据进行分页检索
+      this.pageData = filterData.slice(limit * (page - 1), limit * page)
     },
     handleFilter() {
       this.listQuery.page = 1
-      this.getList()
-    }
+      this.paging()
+    },
 
+    handleCreate() {
+
+    },
+    handleUpdate(row) {
+
+    },
+    handleDelete(row) {
+
+    }
   }
 }
 </script>
